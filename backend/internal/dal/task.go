f99918c2b29b546cd4faf1e72b934f4c02fbad21
st0pcha <stanislav.simakhin@yandex.ru>
2024-12-22 20:07:13 +0300

    added crud for user&task

--- backend/internal/dal/task.go
+++ backend/internal/dal/task.go
@@ -2,6 +2,8 @@ package dal
 
 import (
 	"github.com/google/uuid"
+	"github.com/st0pcha/task-manager/backend/internal/db"
+	"gorm.io/gorm"
 )
 
 type Task struct {
@@ -11,4 +13,20 @@ type Task struct {
 	IsDone  bool      `gorm:"default:false" json:"is_done"`
 	UserID  uuid.UUID `gorm:"not null" json:"user_id"`
 	User    User      `gorm:"foreignKey:UserID" json:"user"`
+}
+
+func CreateTask(task *Task) *gorm.DB {
+	return db.DB.Create(task)
+}
+
+func FindTask(dest interface{}, conds ...interface{}) *gorm.DB {
+	return db.DB.Model(&Task{}).Take(dest, conds...)
+}
+
+func FindTaskByID(dest interface{}, id string) *gorm.DB {
+	return FindTask(dest, "id = ?", &id)
+}
+
+func FindTasksByUserID(dest interface{}, userID uuid.UUID) *gorm.DB {
+	return FindTask(dest, "user_id = ?", &userID)
 }
--- backend/internal/dal/user.go
+++ backend/internal/dal/user.go
@@ -1,9 +1,30 @@
 package dal
 
+import (
+	"github.com/st0pcha/task-manager/backend/internal/db"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	Base
 
 	Email    string `gorm:"unique" json:"email"`
 	Password string `json:"-"`
 	Tasks    []Task `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"tasks"`
+}
+
+func CreateUser(user *User) *gorm.DB {
+	return db.DB.Create(user)
+}
+
+func FindUser(dest interface{}, conds ...interface{}) *gorm.DB {
+	return db.DB.Model(&User{}).Take(dest, conds...)
+}
+
+func FindUserByID(dest interface{}, id string) *gorm.DB {
+	return FindUser(dest, "id = ?", &id)
+}
+
+func FindUserByEmail(dest interface{}, email string) *gorm.DB {
+	return FindUser(dest, "email = ?", &email)
 }
